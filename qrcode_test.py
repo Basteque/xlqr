13b75c68537052e57ab6a0a8d9cd5f16efd35f00
Basteque <82060824+Basteque@users.noreply.github.com>
2021-09-08 23:11:14 +0200

    Revert "Revert "Working prototype""
    This reverts commit 2b151d280105b6de8e63ec7805eb754569411359.

--- qrcode_test.py
+++ qrcode_test.py
@@ -12,47 +12,41 @@ from qrcode.image.styledpil import StyledPilImage
 from qrcode.image.styles.moduledrawers import CircleModuleDrawer
 from qrcode.image.styles.moduledrawers import GappedSquareModuleDrawer
   
-# taking image which user wants 
-# in the QR code center
+# center image
 Logo_link = 'youtube-new-logo3NB.jpg'
   
 logo = Image.open(Logo_link)
   
-# taking base width
-basewidth = 195
+# image width
+basewidth = 190
   
-# adjust image size
+# setting up overlay image + qrcode instance
 wpercent = (basewidth/float(logo.size[0]))
-hsize = int((float(logo.size[1])*float(wpercent)))
+hsize = int((float(logo.size[1])*float(wpercent)))+4
 logo = logo.resize((basewidth, hsize), Image.ANTIALIAS)
 QRcode = qrcode.QRCode(
         error_correction=qrcode.constants.ERROR_CORRECT_H)
-  
-# taking url or text
-url = 'https://bit.ly/AbouKouyateAssyYT'
-  
-# addingg URL or text to QRcode
-QRcode.add_data(url)
-  
-# generating QR code
-QRcode.make()
-  
-# taking color name from user
-QRcolor = 'Black'
-  
-# adding color to QR code
-# QRimg = QRcode.make_image(
-    # fill_color=QRcolor, back_color="white").convert('RGB')
-    
-QRimg = QRcode.make_image(image_factory=StyledPilImage, module_drawer=GappedSquareModuleDrawer())
-  
-# set size of QR code
-pos = ((QRimg.size[0] - logo.size[0]) // 2,
-       (QRimg.size[1] - logo.size[1]) // 2)
-QRimg.paste(logo, pos)
-  
-# save the QR code generated
-QRimg.save('qrCodes/gfg_QR3.png')
-  
-print('QR code generated!')
-# QRimg.show()
+
+def qr_generate(url,name,i):
+    QRcode.clear()
+      
+    QRcode.add_data(url)
+      
+    # Generating qr code with the url
+    QRcode.make()
+    QRimg = QRcode.make_image(image_factory=StyledPilImage, module_drawer=GappedSquareModuleDrawer())
+      
+    # putting overlay image on QR code
+    pos = ((QRimg.size[0] - logo.size[0]) // 2,
+           (QRimg.size[1] - logo.size[1]) // 2)
+    QRimg.paste(logo, pos)
+      
+    # saving the QR code generated
+    # QRimg.save('qrCodes/{}.png'.format(name.replace(" ","_")))
+    filename=name.replace(" ","_")
+    QRimg.save('qrCodes/{}.png'.format(filename))
+    print(filename)
+      
+    print('QR code number {} generated!'.format(i-1))
+    return QRimg
+    # QRimg.show()
