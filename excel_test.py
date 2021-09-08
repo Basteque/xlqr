2b151d280105b6de8e63ec7805eb754569411359
Basteque <82060824+Basteque@users.noreply.github.com>
2021-09-08 23:05:22 +0200

    Revert "Working prototype"
    This reverts commit 7e611273ed7a65fbacf73c1da1ad9ac12989a781.

--- excel_test.py
+++ excel_test.py
@@ -5,23 +5,3 @@ Created on Wed Sep  8 14:14:13 2021
 @author: basil
 """
 
-from openpyxl import load_workbook
-from openpyxl.drawing.image import Image
-import qrcode_test as code
-
-workbook = load_workbook(filename="Classeur.xlsx")
-print(workbook.sheetnames)
-
-sheet = workbook.active
-# print(sheet)
-# print(sheet.cell(1,2).value)
-# print(code.Logo_link)
-sheet.cell(1,3).value = "QR Code"
-for i in range(2,39):
-    name = str(sheet.cell(i,1).value)
-    url = str(sheet.cell(i,2).value)
-    img= code.qr_generate(url,name,i)
-    # sheet.cell(3,i).value=
-    sheet.add_image(img, sheet.cell(i,3))
-    
-# workbook.save("Classeur_final.xlsx")
